e61c2450b815af01f20b1b8e684f16b061cf4ba3
bijinbjames <bijinbjames@gmail.com>
2022-11-24 23:28:41 +0530

    PLay room updated

--- play.py
+++ play.py
@@ -3,7 +3,7 @@ import os
 import time
 import random
 
-def print_slow(str, delay =0.1):
+def print_slow(str, delay =0.01):
     for letter in str:
         sys.stdout.write(letter)
         sys.stdout.flush()
@@ -81,13 +81,21 @@ def entrance():
 
 def play_room():
     fprint("""You have entered the play room.""",1)
-    fprint("""This is the royal game room. You see a pool table at the center of the room with a crumpled paper ball with something written over it. You can go east, or west.""")
+    fprint("""This is the royal game room. You see a pool table at the center of the room with a crumpled paper ball with something written over it. 
+    You can go east, or west.
+    Do you want read the ceumpled paper ball?
+    *) yes
+    *) No""",1)
     while True:
         choice = input("\n>")
         if choice == "east":
             chamber()
         elif choice == "west":
             entrance()
+        elif choice == "yes":
+            print("The paper ball says:") 
+            print_slow("""'To be the powerful witch of the world, Dorothy needs to drink a puppy’s blood.
+             Lock puppy in the magic bound undercroft until sunset' """)
         else:
             fprint(f"Sorry, I didn't understand that. Please try again.")
 
