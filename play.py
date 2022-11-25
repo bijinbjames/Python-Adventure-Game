6d836701801287098c1d70f262c696edfce04782
bijinbjames <bijinbjames@gmail.com>
2022-11-25 20:17:39 +0530

    ommented the code

--- play.py
+++ play.py
@@ -2,7 +2,7 @@ import sys
 import os
 import time
 import random
-
+"""This function slows down the print function so that it looks like the computer is typing."""
 def print_slow(str, delay =0.01, delay2 = 1):
     for letter in str:
         sys.stdout.write(letter)
@@ -10,35 +10,21 @@ def print_slow(str, delay =0.01, delay2 = 1):
         time.sleep(delay)
     print("\n")
     time.sleep(delay2)
-
+"""This function clears the console"""
 def reset_console():
     print("\n")
     os.system("cls")
-
+"""This function puts a delay between each line of text"""
 def fprint(str, delay =0.1):
     print("\n"+str)
     time.sleep(delay)
 
-def sprint(str, delay =0.1):
-    print(str)
-    time.sleep(delay)
-
-    player = {
-        "name": "Player",
-        "items": [],
-        "location": "start"
-    }
-
-""" This is the main room of the game. It is where the player will 
-    start the game. It is also where the player will be able to 
-    access all other rooms in the game."""
-
-
+"""This is the initial state of items in the game"""
 knife = False
 key = False
 dorothy_killed = False
 
-
+"""This function is the welcome screen"""
 def print_welcome():
     print_slow("""
     HI!
@@ -68,15 +54,17 @@ def print_welcome():
         else:
             fprint("Don't know what you mean.")
 
+
+"""This function is the enterence i.e the Great Hall"""
 def entrance():
     print_slow("""You are now in the Great Hall.""")
     print_slow("""All you see is a wide open empty hall with a big crystal chandelier hanging from the ceiling.""")
     print_slow("""You can go 'north', 'south', 'east', or 'west'.""")
     print_slow("""Where do you want to go?""")
-    
-    
+
     while True:
         choice = input("\n>")
+        """This code determines what instance of the gallery function to run"""
         if choice =="north" and dorothy_killed == False:
             gallery()
         elif choice =="north" and dorothy_killed == True:
@@ -376,5 +364,6 @@ def quit_game():
     
 
 print_welcome()
+"""https://i.kym-cdn.com/entries/icons/mobile/000/028/021/work.jpg"""
 
 
